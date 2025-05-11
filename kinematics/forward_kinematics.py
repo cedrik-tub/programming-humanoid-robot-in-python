58779bd649997a99328341f641cd732b09a9d3d4
Cedrik <cedrik.r@my.com>
2025-05-11 16:14:32 +0200

    Kinematics

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -16,14 +16,59 @@
     2. Please use radians and meters as unit.
 '''
 
-# add PYTHONPATH
 import os
 import sys
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', 'joint_control'))
 
+from numpy import sin, cos, pi, deg2rad
 from numpy.matlib import matrix, identity
 
 from recognize_posture import PostureRecognitionAgent
+
+def Rx(angle_rad):
+    c = cos(angle_rad)
+    s = sin(angle_rad)
+    return matrix([[1, 0, 0, 0],
+                   [0, c, -s, 0],
+                   [0, s,  c, 0],
+                   [0, 0,  0, 1]], dtype=float)
+
+def Ry(angle_rad):
+    c = cos(angle_rad)
+    s = sin(angle_rad)
+    return matrix([[c, 0, s, 0],
+                   [0, 1, 0, 0],
+                   [-s,0, c, 0],
+                   [0, 0, 0, 1]], dtype=float)
+
+def Rz(angle_rad):
+    c = cos(angle_rad)
+    s = sin(angle_rad)
+    return matrix([[c, -s,0, 0],
+                   [s,  c, 0, 0],
+                   [0, 0,  1, 0],
+                   [0, 0,  0, 1]], dtype=float)
+
+def Trans(x, y, z):
+    return matrix([[1, 0, 0, x],
+                   [0, 1, 0, y],
+                   [0, 0, 1, z],
+                   [0, 0, 0, 1]], dtype=float)
+
+NECK_OFFSET_Z = 0.1265
+SHOULDER_OFFSET_Y = 0.098
+SHOULDER_OFFSET_Z = 0.100
+UPPER_ARM_LENGTH = 0.105
+ELBOW_OFFSET_Y = 0.015
+LOWER_ARM_LENGTH = 0.05595
+HAND_OFFSET_X = 0.05775
+HAND_OFFSET_Z = 0.01231
+
+HIP_OFFSET_Y = 0.050
+HIP_OFFSET_Z = 0.085
+THIGH_LENGTH = 0.100
+TIBIA_LENGTH = 0.10290
+FOOT_HEIGHT = 0.04519
 
 
 class ForwardKinematicsAgent(PostureRecognitionAgent):
@@ -33,44 +78,87 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
                  player_id=0,
                  sync_mode=True):
         super(ForwardKinematicsAgent, self).__init__(simspark_ip, simspark_port, teamname, player_id, sync_mode)
+        
+        self.joint_names.extend(['LFoot', 'RFoot', 'LArmEffector', 'RArmEffector'])
         self.transforms = {n: identity(4) for n in self.joint_names}
 
-        # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
-                       # YOUR CODE HERE
-                       }
+        self.chains = {
+            'Head': ['HeadYaw', 'HeadPitch'],
+            'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw', 'LArmEffector'],
+            'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw', 'RArmEffector'],
+            'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll', 'LFoot'],
+            'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll', 'RFoot']
+        }
 
     def think(self, perception):
         self.forward_kinematics(perception.joint)
         return super(ForwardKinematicsAgent, self).think(perception)
 
     def local_trans(self, joint_name, joint_angle):
-        '''calculate local transformation of one joint
-
-        :param str joint_name: the name of joint
-        :param float joint_angle: the angle of joint in radians
-        :return: transformation
-        :rtype: 4x4 matrix
-        '''
         T = identity(4)
-        # YOUR CODE HERE
-
+        if joint_name == 'HeadYaw':
+            T = Trans(0, 0, NECK_OFFSET_Z) * Rz(joint_angle)
+        elif joint_name == 'HeadPitch':
+            T = Ry(joint_angle)
+        elif joint_name == 'LShoulderPitch':
+            T = Trans(0, SHOULDER_OFFSET_Y, SHOULDER_OFFSET_Z) * Ry(joint_angle)
+        elif joint_name == 'LShoulderRoll':
+            T = Rz(joint_angle)
+        elif joint_name == 'LElbowYaw':
+            T = Trans(UPPER_ARM_LENGTH, ELBOW_OFFSET_Y, 0) * Rx(joint_angle)
+        elif joint_name == 'LElbowRoll':
+            T = Rz(joint_angle)
+        elif joint_name == 'LWristYaw':
+            T = Trans(LOWER_ARM_LENGTH, 0, 0) * Rx(joint_angle)
+        elif joint_name == 'LArmEffector':
+             T = Trans(HAND_OFFSET_X, 0, -HAND_OFFSET_Z)
+        elif joint_name == 'RShoulderPitch':
+            T = Trans(0, -SHOULDER_OFFSET_Y, SHOULDER_OFFSET_Z) * Ry(joint_angle)
+        elif joint_name == 'RShoulderRoll':
+            T = Rz(joint_angle)
+        elif joint_name == 'RElbowYaw':
+            T = Trans(UPPER_ARM_LENGTH, -ELBOW_OFFSET_Y, 0) * Rx(joint_angle)
+        elif joint_name == 'RElbowRoll':
+            T = Rz(joint_angle)
+        elif joint_name == 'RWristYaw':
+            T = Trans(LOWER_ARM_LENGTH, 0, 0) * Rx(joint_angle)
+        elif joint_name == 'RArmEffector':
+             T = Trans(HAND_OFFSET_X, 0, -HAND_OFFSET_Z)
+        elif joint_name == 'LHipYawPitch':
+            T = Trans(0, HIP_OFFSET_Y, -HIP_OFFSET_Z) * Rz(joint_angle)
+        elif joint_name == 'LHipRoll':
+            T = Rx(joint_angle)
+        elif joint_name == 'LHipPitch':
+            T = Ry(joint_angle)
+        elif joint_name == 'LKneePitch':
+            T = Trans(0, 0, -THIGH_LENGTH) * Ry(joint_angle)
+        elif joint_name == 'LAnklePitch':
+            T = Trans(0, 0, -TIBIA_LENGTH) * Ry(joint_angle)
+        elif joint_name == 'LAnkleRoll':
+            T = Rx(joint_angle)
+        elif joint_name == 'LFoot':
+            T = Trans(0, 0, -FOOT_HEIGHT)
+        elif joint_name == 'RHipYawPitch':
+            T = Trans(0, -HIP_OFFSET_Y, -HIP_OFFSET_Z) * Rz(joint_angle)
+        elif joint_name == 'RHipRoll':
+            T = Rx(joint_angle)
+        elif joint_name == 'RHipPitch':
+            T = Ry(joint_angle)
+        elif joint_name == 'RKneePitch':
+            T = Trans(0, 0, -THIGH_LENGTH) * Ry(joint_angle)
+        elif joint_name == 'RAnklePitch':
+            T = Trans(0, 0, -TIBIA_LENGTH) * Ry(joint_angle)
+        elif joint_name == 'RAnkleRoll':
+            T = Rx(joint_angle)
+        elif joint_name == 'RFoot':
+            T = Trans(0, 0, -FOOT_HEIGHT)
         return T
 
     def forward_kinematics(self, joints):
-        '''forward kinematics
-
-        :param joints: {joint_name: joint_angle}
-        '''
-        for chain_joints in self.chains.values():
-            T = identity(4)
+        for chain_name, chain_joints in self.chains.items():
+            T_global = identity(4)
             for joint in chain_joints:
-                angle = joints[joint]
+                angle = joints.get(joint, 0.0)
                 Tl = self.local_trans(joint, angle)
-                # YOUR CODE HERE
-
-                self.transforms[joint] = T
-
-if __name__ == '__main__':
-    agent = ForwardKinematicsAgent()
-    agent.run()
+                T_global = T_global * Tl
+                self.transforms[joint] = T_global
--- kinematics/inverse_kinematics.py
+++ kinematics/inverse_kinematics.py
@@ -10,33 +10,135 @@
 '''
 
 
-from forward_kinematics import ForwardKinematicsAgent
-from numpy.matlib import identity
+import numpy as np
+from math import acos, asin, atan2, sqrt, pi, degrees
 
+from forward_kinematics import ForwardKinematicsAgent, Trans, Rx, Ry, Rz
+from forward_kinematics import THIGH_LENGTH, TIBIA_LENGTH, HIP_OFFSET_Y, HIP_OFFSET_Z, FOOT_HEIGHT
 
 class InverseKinematicsAgent(ForwardKinematicsAgent):
-    def inverse_kinematics(self, effector_name, transform):
-        '''solve the inverse kinematics
+    def inverse_kinematics(self, effector_name, transform_torso_to_foot):
+        joint_angles_rad = [0.0] * 6
 
-        :param str effector_name: name of end effector, e.g. LLeg, RLeg
-        :param transform: 4x4 transform matrix
-        :return: list of joint angles
-        '''
-        joint_angles = []
-        # YOUR CODE HERE
-        return joint_angles
+        if effector_name != 'LLeg' and effector_name != 'RLeg':
+            print(f"Inverse Kinematics not implemented for {effector_name}")
+            return joint_angles_rad
+
+        l_upper_leg = THIGH_LENGTH
+        l_lower_leg = TIBIA_LENGTH
+        
+        current_hip_offset_y = HIP_OFFSET_Y
+        if effector_name == 'RLeg':
+            current_hip_offset_y = -HIP_OFFSET_Y
+
+        T_torso_to_hip_yaw_pitch_origin = Trans(0, current_hip_offset_y, -HIP_OFFSET_Z)
+        T_hip_yaw_pitch_origin_to_foot = np.linalg.inv(T_torso_to_hip_yaw_pitch_origin) * transform_torso_to_foot
+        
+        p_AR_H0 = np.array(T_hip_yaw_pitch_origin_to_foot[0:3, 3]).flatten()
+
+        rot_angle_hip_ortho = pi/4
+        if effector_name == 'RLeg':
+            rot_angle_hip_ortho = -pi/4
+        
+        Rx_hip_ortho_mat = np.array(Rx(rot_angle_hip_ortho)[0:3, 0:3])
+
+        p_AR_H_ortho = Rx_hip_ortho_mat @ p_AR_H0
+        x, y, z = p_AR_H_ortho[0], p_AR_H_ortho[1], p_AR_H_ortho[2]
+
+        l_trans_sq = x*x + y*y + z*z
+        l_trans = sqrt(l_trans_sq)
+
+        cos_gamma_knee_arg = (l_upper_leg**2 + l_lower_leg**2 - l_trans_sq) / (2 * l_upper_leg * l_lower_leg)
+        if cos_gamma_knee_arg > 1.0: cos_gamma_knee_arg = 1.0
+        if cos_gamma_knee_arg < -1.0: cos_gamma_knee_arg = -1.0
+        gamma_knee = acos(cos_gamma_knee_arg)
+        delta_knee = gamma_knee # Aldebaran Doku: positiver Winkel ist Beugung
+        joint_angles_rad[3] = delta_knee
+
+        val_acos_foot1_arg = (l_lower_leg**2 + l_trans_sq - l_upper_leg**2) / (2 * l_lower_leg * l_trans)
+        if l_trans == 0: # Vermeide Division durch Null, falls Fuß am Hüft-Ursprung
+             val_acos_foot1_arg = 0 # Führt zu pi/2, was wahrscheinlich nicht ideal ist, aber Singularität vermeidet
+        elif val_acos_foot1_arg > 1.0: val_acos_foot1_arg = 1.0
+        elif val_acos_foot1_arg < -1.0: val_acos_foot1_arg = -1.0
+        delta_foot_pitch1 = acos(val_acos_foot1_arg)
+
+        delta_foot_pitch2 = atan2(x, sqrt(y**2 + z**2) if (y**2 + z**2) > 1e-9 else 1e-9) # Vermeide sqrt(0)
+        delta_foot_roll = atan2(y, z if abs(z) > 1e-9 else 1e-9 * np.sign(z) if z!=0 else 1e-9 ) #Vermeide atan2(y,0) für z=0
+
+        joint_angles_rad[4] = -(delta_foot_pitch1 + delta_foot_pitch2) # NAO Konvention: negativer Pitch neigt Fuß nach unten
+        joint_angles_rad[5] = delta_foot_roll                    
+        
+        R_H0_AR_original = np.array(T_hip_yaw_pitch_origin_to_foot[0:3, 0:3])
+        R_H_ortho_AR = Rx_hip_ortho_mat @ R_H0_AR_original
+        
+        R_Thigh_Knee = np.array(Ry(delta_knee)[0:3,0:3])
+        R_Knee_AnkleP = np.array(Ry(joint_angles_rad[4])[0:3,0:3])
+        R_AnkleP_AnkleR = np.array(Rx(joint_angles_rad[5])[0:3,0:3])
+        
+        R_Thigh_AR = R_Thigh_Knee @ R_Knee_AnkleP @ R_AnkleP_AnkleR
+
+        R_HT = R_H_ortho_AR @ np.linalg.inv(R_Thigh_AR)
+
+        # Euler-Winkel-Extraktion aus R_HT (Z-X-Y Reihenfolge für HipYawPitch, HipRoll', HipPitch')
+        # R_HT = Rz(delta_hip_yaw) * Rx(delta_hip_roll_prime) * Ry(delta_hip_pitch)
+        
+        # sin(roll') = R_HT[2,1]
+        sin_delta_hip_roll_prime_val = R_HT[2,1]
+        if sin_delta_hip_roll_prime_val > 1.0: sin_delta_hip_roll_prime_val = 1.0
+        if sin_delta_hip_roll_prime_val < -1.0: sin_delta_hip_roll_prime_val = -1.0
+        delta_hip_roll_prime = asin(sin_delta_hip_roll_prime_val)
+
+        cos_delta_hip_roll_prime = cos(delta_hip_roll_prime)
+
+        if abs(cos_delta_hip_roll_prime) < 1e-6: # Gimbal Lock Fall
+            delta_hip_yaw = 0 # Kann nicht eindeutig bestimmt werden, setze zu 0
+            delta_hip_pitch = atan2(R_HT[0,2], R_HT[0,0]) # Oder eine andere Konvention
+        else:
+            delta_hip_pitch = atan2(-R_HT[2,0] / cos_delta_hip_roll_prime, R_HT[2,2] / cos_delta_hip_roll_prime)
+            delta_hip_yaw = atan2(-R_HT[0,1] / cos_delta_hip_roll_prime, R_HT[1,1] / cos_delta_hip_roll_prime)
+
+        joint_angles_rad[0] = delta_hip_yaw     
+        joint_angles_rad[1] = delta_hip_roll_prime - rot_angle_hip_ortho 
+        joint_angles_rad[2] = delta_hip_pitch  
+
+        if effector_name == 'RLeg':
+            joint_angles_rad[0] *= -1 
+            joint_angles_rad[1] *= -1 
+            joint_angles_rad[5] *= -1 
+            
+        return joint_angles_rad
 
     def set_transforms(self, effector_name, transform):
-        '''solve the inverse kinematics and control joints use the results
-        '''
-        # YOUR CODE HERE
-        self.keyframes = ([], [], [])  # the result joint angles have to fill in
+        joint_angles_rad = self.inverse_kinematics(effector_name, transform)
+        
+        joint_angles_deg = [degrees(angle) for angle in joint_angles_rad]
+        print(f"Calculated angles for {effector_name} (deg): {joint_angles_deg}")
+
+        if effector_name == 'LLeg':
+            leg_joint_names = ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll']
+        elif effector_name == 'RLeg':
+            leg_joint_names = ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll']
+        else:
+            return
+
+        if joint_angles_rad:
+            values_for_keyframes = [joint_angles_rad]
+            times_for_keyframes = [[1.0] * len(joint_angles_rad)] 
+            self.keyframes = (leg_joint_names, values_for_keyframes, times_for_keyframes)
+
 
 if __name__ == '__main__':
     agent = InverseKinematicsAgent()
-    # test inverse kinematics
-    T = identity(4)
-    T[-1, 1] = 0.05
-    T[-1, 2] = -0.26
-    agent.set_transforms('LLeg', T)
+    
+    target_x_torso = 0.0
+    target_y_torso_left_foot = HIP_OFFSET_Y + 0.05
+    target_z_torso = -0.26 
+
+    T_LLeg_target = np.array(Trans(target_x_torso, target_y_torso_left_foot, target_z_torso))
+    T_LLeg_target[0:3, 0:3] = np.identity(3)
+
+    print("Ziel-Transformation für LLeg (Torso zu Fuß):")
+    print(T_LLeg_target)
+    agent.set_transforms('LLeg', T_LLeg_target)
+    
     agent.run()
